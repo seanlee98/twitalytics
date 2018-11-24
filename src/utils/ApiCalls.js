fbb6592f3e50591226983161ddcc9583f5ed7b6c
Sean Lee <s386lee@uwaterloo.ca>
2018-11-24 12:15:09 -0500

    map api call to data graph visual

--- src/utils/ApiCalls.js
+++ src/utils/ApiCalls.js
@@ -1,5 +1,7 @@
-export const fetchTwitterData = async () => {
-  const response = await fetch("https:localhost:5000/api/twitter_query");
+export const fetchTwitterData = async query => {
+  const response = await fetch(
+    `http://localhost:5000/api/twitter_query?query=${query}`
+  );
   if (response.status >= 400) {
     throw new Error("Error fetching twitter data");
   } else {
